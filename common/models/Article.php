ecd88b776aad62b2a4b511ad26beb17054a9c5a8
Igor <crocusik@mail.ru>
2023-04-18 16:51:36 +0500

    Модель Article

---
+++ common/models/Article.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace common\models;
+
+use yii\db\ActiveRecord;
+
+/**
+ * This is the model class for table "production".
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $article
+ */
+class Article extends ActiveRecord
+{
+
+    /**
+     * @inheritdoc
+     */
+    public static function tableName(): string
+    {
+        return 'articles';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules(): array
+    {
+        return [
+            [['name', 'article'], 'required'],
+            [['name', 'article'], 'string'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Название Статьи',
+            'article' => 'Статья',
+        ];
+    }
+
+}
