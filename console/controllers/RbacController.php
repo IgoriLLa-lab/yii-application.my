c16f4927e10c94060df00886b525fa610a7f0ac5
Igor <crocusik@mail.ru>
2023-04-18 14:44:35 +0500

    RbacController метод init (yii rbac/init)

---
+++ console/controllers/RbacController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace console\controllers;
+
+use Exception;
+use yii\console\Controller;
+
+class RbacController extends Controller
+{
+
+    /**
+     * @throws Exception
+     */
+    public function actionInit()
+    {
+
+        $auth = \Yii::$app->authManager;
+        $auth->removeAll();
+
+        $adminPanel = $auth->createPermission('adminPanel');
+        $adminPanel->description = 'Панель администратора';
+        $auth->add($adminPanel);
+
+        $createArticle = $auth->createPermission('createArticle');
+        $createArticle->description = 'Create a article';
+        $auth->add($createArticle);
+
+        $updateArticle = $auth->createPermission('updateArticle');
+        $updateArticle->description = 'Update article';
+        $auth->add($updateArticle);
+
+        $deleteArticle = $auth->createPermission('deleteArticle');
+        $deleteArticle->description = 'Delete post';
+        $auth->add($deleteArticle);
+
+        $user = $auth->createRole('user');
+        $user->description = 'Пользователь';
+        $auth->add($user);
+        $auth->addChild($user, $createArticle);
+
+        $administrator = $auth->createRole('administrator');
+        $administrator->description = 'Администратор';
+        $auth->add($administrator);
+        $auth->addChild($administrator, $updateArticle);
+        $auth->addChild($administrator, $deleteArticle);
+    }
+
+}
