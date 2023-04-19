40d64f7bc5574d1be9a7e96da733d4583ffdc054
Igor <crocusik@mail.ru>
2023-04-19 13:18:22 +0500

    доработка ArticleController.php

--- frontend/controllers/ArticleController.php
+++ frontend/controllers/ArticleController.php
@@ -3,6 +3,8 @@
 namespace frontend\controllers;
 
 use common\models\Article;
+use common\models\User;
+use Yii;
 use yii\web\Controller;
 
 class ArticleController extends Controller
@@ -15,7 +17,15 @@ class ArticleController extends Controller
      */
     public function actionCreateArticle() : string
     {
+
         $model = new Article();
+
+        if ($model->load(Yii::$app->request->post())){
+            $model->user_id = Yii::$app->user->getId();
+            $model->save();
+
+            $this->refresh();
+        }
 
         return $this->render('create-article', ['model' => $model]);
     }
