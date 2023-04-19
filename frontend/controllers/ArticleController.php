0ac041dfaf99673d0065de42f69d0e286fde8d27
Igor <crocusik@mail.ru>
2023-04-19 12:54:28 +0500

    создаем ArticleController.php

--- frontend/controllers/ArticleController.php
+++ frontend/controllers/ArticleController.php
@@ -3,8 +3,21 @@
 namespace frontend\controllers;
 
 use common\models\Article;
+use yii\web\Controller;
 
-class ArticleController
+class ArticleController extends Controller
 {
+
+    /**
+     * Создание статьи и отрисовка формы
+     *
+     * @return string
+     */
+    public function actionCreateArticle() : string
+    {
+        $model = new Article();
+
+        return $this->render('create-article', ['model' => $model]);
+    }
 
 }
