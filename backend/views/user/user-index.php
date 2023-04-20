02789de5dd59e0f630f8733f5420be2fe3575b1d
Igor <crocusik@mail.ru>
2023-04-20 17:08:28 +0500

    index

--- backend/views/user/user-index.php
+++ backend/views/user/user-index.php
@@ -1,8 +1,10 @@
 <?php
 
-use yii\bootstrap5\Html;
 use yii\grid\GridView;
+use yii\helpers\Html;
 use yii\helpers\Url;
+
+
 
 echo GridView::widget([
     'dataProvider' => $dataProvider,
@@ -12,26 +14,43 @@ echo GridView::widget([
         'username',
         'email',
 
-        ['class' => 'yii\grid\ActionColumn',
+        [
+            'class' => 'yii\grid\ActionColumn',
             'template' => '{update} {delete}',
-            'buttons'=> [
 
-                'update' => function ($url) {
-                    $url = Url::to(['update-user']);
-                    return Html::a('update', $url, [
-                        'title' => Yii::t('app', 'user-update'),
-                    ]);
-                },
-                'delete' => function ($url) {
-                    $url = Url::to(['delete-user']);
-                    return Html::a('delete', $url, [
-                        'title' => Yii::t('app', 'user-delete'),
-                    ]);
-                },
+            ]
 
-            ],
 
-        ],
+//        ['class' => 'yii\grid\ActionColumn',
+//            'template' => '{update} {delete}',
+//            'header' => 'Actions',
+//            'headerOptions' => ['style' => 'color:#337ab7'],
+//            'buttons' => [
+//
+//                'update' => function ($url) {
+//                    $url = Url::toRoute(["update-user"]);
+//                    return Html::a('update', $url, [
+//                        'title' => Yii::t('app', 'user-update'),
+//                    ]);
+//                },
+//                'delete' => function ($url) {
+//                    $url = Url::toRoute(['deleteuser']);
+//                    return Html::a('delete', $url, [
+//                        'title' => Yii::t('app', 'user-delete'),
+//                    ]);
+//                },
+////                'urlCreator' => function ($action, $model, $key, $index) {
+////                    if ($action === 'update-user') {
+////                        $url ='index.php?r=user%2Fupdate-user='.Yii::$app->request->get("id")."&id=".$model->id;
+////                        return $url;
+////                    }
+////
+////
+////                }
+//
+//            ],
+//
+//        ],
 
     ]
 ]);
