463364749448ea3dfb2df93b31b6f922a22792f8
Igor <crocusik@mail.ru>
2023-04-20 19:07:14 +0500

    создан

---
+++ backend/controllers/ArticleController.php
@@ -0,0 +1,68 @@
+cj<?php
+
+namespace backend\controllers;
+
+use common\models\Article;
+use common\models\User;
+use Throwable;
+use Yii;
+use yii\data\ActiveDataProvider;
+use yii\db\StaleObjectException;
+use yii\web\Controller;
+
+class ArticleController extends Controller
+{
+
+    /**
+     * Displays homepage.
+     *
+     * @return string
+     */
+    public function actionArticleIndex()
+    {
+        $dataProvider = new ActiveDataProvider([
+            'query' => Article::find()
+        ]);
+
+        return $this->render('index', ['dataProvider' => $dataProvider]);
+    }
+
+        public function actionView($id)
+    {
+        $model = Article::findOne($id);
+
+        return $this->render('view', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * @throws StaleObjectException
+     * @throws Throwable
+     */
+    public function actionDelete($id)
+    {
+        $model = Article::findOne($id);
+
+        if ($model) {
+            $model->delete();
+
+            return $this->redirect('view');
+        }
+        return '';
+    }
+
+    public function actionUpdate(int $id)
+    {
+        $model = Article::findOne($id);
+
+        if ($model->load(Yii::$app->request->post())) {
+            $model->update();
+
+            return $this->render('view', ['model' => $model]);
+        }
+
+        return $this->render('update', ['model' => $model]);
+    }
+
+}
